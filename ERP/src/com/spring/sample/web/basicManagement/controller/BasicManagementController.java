e89437562eef412f74f4c4764a9f0be517028073
fidrn456 <fidrn456@naver.com>
2016-11-25 10:00:06 +0900

    2016.11.25일 오전10시 강인구 (기본관리 컨트롤러 추가)

--- ERP/src/com/spring/sample/web/basicManagement/controller/BasicManagementController.java
+++ ERP/src/com/spring/sample/web/basicManagement/controller/BasicManagementController.java
@@ -8,116 +8,116 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BasicManagementController {
-	   /*@RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/BankList")
+	   public ModelAndView BankList(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Bank/BankList");
 	      return modelAndView;
 	   } //컨트롤러의 가장 기본적인 형태
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/BankRegister")
+	   public ModelAndView BankRegister(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Bank/BankRegister");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/CompanyRegister")
+	   public ModelAndView CompanyRegister(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Company/CompanyRegister");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/CustomerList")
+	   public ModelAndView CustomerListt(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Customer/CustomerList");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/CustomerRegister")
+	   public ModelAndView CustomerRegister(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Customer/CustomerRegister");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/DeptCode")
+	   public ModelAndView DeptCode(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Dept/DeptCode");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/DeptList")
+	   public ModelAndView DeptList(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Dept/DeptList");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/DeptRegister")
+	   public ModelAndView DeptRegister(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Dept/DeptRegister");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/GroupCodeRegister")
+	   public ModelAndView GroupCodeRegister(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement//GroupCode/GroupCodeRegister");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/ItemList")
+	   public ModelAndView ItemList(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Item/ItemList");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/ItemRegister")
+	   public ModelAndView ItemRegister(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Item/ItemRegister");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/JoinCompany")
+	   public ModelAndView JoinCompany(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Member/JoinCompany");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/MemList")
+	   public ModelAndView MemList(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Member/MemList");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/MemPosition")
+	   public ModelAndView MemPosition(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Member/MemPosition");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/MemRank")
+	   public ModelAndView MemRank(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Member/MemRank");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/MemRegister")
+	   public ModelAndView MemRegister(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Member/MemRegister");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/SubjectSeach")
+	   public ModelAndView SubjectSeach(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Subject/SubjectSeach");
 	      return modelAndView;
 	   }
-	   @RequestMapping(value="/test6")
-	   public ModelAndView Test(HttpServletRequest request, ModelAndView modelAndView) {
+	   @RequestMapping(value="/SubjectRegister")
+	   public ModelAndView SubjectRegister(HttpServletRequest request, ModelAndView modelAndView) {
 		   
-	      modelAndView.setViewName("basicManagement/basicManagement");
+	      modelAndView.setViewName("basicManagement/Subject/SubjectRegister");
 	      return modelAndView;
-	   }*/
+	   }
 	   @RequestMapping(value="/SubjectList")
-	   public ModelAndView SubjectSearchList(HttpServletRequest request, ModelAndView modelAndView) {
+	   public ModelAndView SubjectList(HttpServletRequest request, ModelAndView modelAndView) {
 		   
 	      modelAndView.setViewName("basicManagement/Subject/SubjectList");
 	      return modelAndView;
